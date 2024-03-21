02e82f71f7375a66e7d1b230ff6a1410c5ddfcce
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-03-21 14:06:41 +0800

    Made some changes on their structures

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-// import React from 'react';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-// function App() {
-//   return (
-//     <h1></h1>
-//   );
-// }
+const App = () => {
+    return(
+        <>
+            <Header />
+            <Footer />
+        </>
 
-// export default App;
+    )
+}
+
+export default App
