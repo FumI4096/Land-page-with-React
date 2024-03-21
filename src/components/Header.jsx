18564f0563135248ab428d6751a42fcf8100c0b4
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-03-21 08:33:41 +0800

    Added styles in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import style from '../style.module.css';
 
 const Header = () => {
+    const styleQuote = {
+        fontFamily: 'Edu NSW ACT Foundation',
+        color: '#fff',
+        zIndex: '5',
+        fontSize: '5rem',
+    };
     return(
         <>
             <header className={style.layout}>
-                <h1>Satisfaction at best</h1>
+                <h1 style={styleQuote}>Satisfaction at best</h1>
             </header>
         </>
     );
