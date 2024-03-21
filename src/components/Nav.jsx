42606933aa7804a339851c4658720a0b0b27a71b
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-03-21 08:33:12 +0800

    Added inline styles in navigation bars

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 
 const Nav = () => {
+
+    const styleTabs = {
+        listStyle: 'none',
+        display: 'flex',
+        padding: '0',
+    };
+
+    const styleTitle = {
+        marginRight: 'auto',
+        color: '#fff',
+    };
+
+    const styleLinks = {
+        textDecoration: 'none',
+        height: '100%',
+        display: styleTabs.display,
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: styleTitle.color,
+        marginLeft: '50px',
+        fontSize: '1.1rem',
+    };
+
     return(
         <>
-            <h2>VibeChecks</h2>
-            <ul>
-                <a href="#home" id="">
+            <h2 style={styleTitle}>VibeChecks</h2>
+            <ul style={styleTabs}>
+                <a href="#home" style={styleLinks}>
                     <li>Home</li>
                 </a>
-                <a href="#about" id="">
+                <a href="#about" style={styleLinks}>
                     <li>About</li>
                 </a>
-                <a href="#contact" id="">
+                <a href="#contact" style={styleLinks}>
                     <li>Contact</li>
                 </a>
             </ul>
