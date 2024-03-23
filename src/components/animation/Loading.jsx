d6f45fcac3ccaa1cc7a240111581f7e3a66329b6
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-03-23 21:52:02 +0800

    Added updates for the loading animation

---
+++ src/components/animation/Loading.jsx
@@ -0,0 +1,52 @@
+// import { useState, useEffect } from "react";
+// import ClipLoader from "react-spinners/ClipLoader";
+import { FaCheck } from "react-icons/fa6";
+import style from './loading.module.css';
+
+
+function Loading() {
+    /*const [loading, setLoading] = useState(true);
+    const [delayTime, setDelayTime] = useState(2000);
+
+    const override = {
+        display: "block",
+        margin: "0 auto",
+        borderColor: "red",
+    };
+    
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setLoading(false);
+        }, delayTime - 1200);
+
+        return () => clearTimeout(timeout);
+
+    }, [setDelayTime]); 
+
+    return (
+        <div className="sweet-loading">
+
+            <ClipLoader
+                // color={color}
+                loading={loading}
+                cssOverride={override}
+                size={150}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
+        </div>
+    );*/
+
+    return (
+        <div className={style.LoadingBar}>
+            <div className={style.LoadingWrap}>
+                <FaCheck className={style.loadingIcon} />
+            </div>
+        </div>
+
+    );
+    
+}
+
+export default Loading;
