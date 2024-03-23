98bd2729b3363066c3a39d3a973cb4d7bf206cf5
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-03-23 21:54:53 +0800

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Loading from './components/animation/Loading';
 
 const App = () => {
     return(
         <>
+            <Loading />
             <Header />
             <Footer />
         </>
