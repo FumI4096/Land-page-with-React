ac454f76bcf7cc2d60a5a51c35b5bea95181919d
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-03-24 22:50:25 +0800

    Added contents.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Loading from './components/animation/Loading';
+import Contents from './components/Contents';
 
 const App = () => {
     return(
         <>
             <Loading />
             <Header />
+            <Contents />
             <Footer />
         </>
     );
